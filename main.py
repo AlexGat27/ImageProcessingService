e082c89876b3c4a3e292c1eaae222eb6d66d94e2
SanGat2 <aleksandrgataulin745@gmail.com>
2024-01-11 01:17:18 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify, abort
+from src.Controlllers.MediaProcessingCtrl import MediaProcessingCtrl
+import base64
+import numpy as np
+
+app = Flask(__name__)
+port = 5000
+
+@app.post('/imageProcessing')
+def process_media():
+    mediaCtrl = MediaProcessingCtrl()
+    data2send = mediaCtrl.process_media(request)
+    return jsonify(data2send)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=port)
