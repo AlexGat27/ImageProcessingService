8b5603fe1edf7e49049fa7470bffa012bbe26e6b
AlexGat27 <aleksandrgataulin745@gmail.com>
2024-01-24 13:57:23 +0300

    Web обработка ошибок

--- main.py
+++ main.py
@@ -1,62 +1,59 @@
-from flask import Flask, request, jsonify, abort
+from flask import Flask, request, jsonify, make_response
+import threading
 from src.Controllers.UnityProcessingCtrl import UnityProcessingCtrl as UnitCtrl
 from src.Controllers.WebImageProcessingCtrl import WebImageProcessingCtrl as WebCtrl
 from src.Controllers.AppProcessingCtrl import AppProcessingCtrl as AppCtrl
 
-app = Flask(__name__)
-port = 6000
+geosystemMobileApp = Flask("GeosystemMobileApp")
+unityApp = Flask("UnityApp")
+tkinterApp = Flask("TkinterApp")
+portMobile = 6001
+portUnity = 6000
+portTkinter = 6002
 
-@app.post('/imageProcessing')
-def ProcessImage():
-    platform = request.args.get("platform")
-    print(platform)
-    if platform == "Unity":
-        print("Get request from Unity")
-        unitCtrl = UnitCtrl()
-        data2send = unitCtrl.MediaProcessing(request)
-    elif platform == "GeosystemApp":
-        print("Get request from GeosystemApp")
-        webCtrl = WebCtrl()
-        data2send = webCtrl.MediaProcessing(request)
-    elif platform == "Application":
-        print('Get request from Application')
-        appCtrl = AppCtrl("ImageApp")
-        data2send = appCtrl.MediaProcessing(request, True)
-    else: 
-        data2send = {
-            'status': 400,
-            'message': "Неверные параметры запроса",
-        }
+@geosystemMobileApp.post('/imageProcessing')
+def ImageProcessingMobile():
+    print("Get request from GeosystemApp")
+    webCtrl = WebCtrl()
+    response = webCtrl.MediaProcessing(request)
+    if response['status'] >= 400: 
+        return make_response(response['message'], response['status'])
+    else:
+        return jsonify(response)
+
+@unityApp.post('/imageProcessing')
+def ImageProcessingUnity():
+    print("Get request from Unity")
+    unitCtrl = UnitCtrl()
+    data2send = unitCtrl.MediaProcessing(request)
     return jsonify(data2send)
 
-@app.post('/videoProcessing')
+@tkinterApp.post('/imageProcessing')
+def ImageProcessingTkinter():
+    print('Get request from Application')
+    appCtrl = AppCtrl("ImageApp")
+    data2send = appCtrl.MediaProcessing(request, True)
+    return jsonify(data2send)
+    
+@tkinterApp.post('/videoProcessing')
 def ProcessVideo():
-    platform = request.args.get("platform")
-    if platform == "Application":
-        print('Get request from Application')
-        appCtrl = AppCtrl("VideoApp")
-        data2send = appCtrl.MediaProcessing(request, False)
-    else: 
-        data2send = {
-            'status': 400,
-            'message': "Неверные параметры запроса",
-        }
+    print('Get request from Application')
+    appCtrl = AppCtrl("VideoApp")
+    data2send = appCtrl.MediaProcessing(request, False)
     return jsonify(data2send)
 
-@app.post('/videoSplit')
+@tkinterApp.post('/videoSplit')
 def SplitVideo():
-    platform = request.args.get("platform")
-    if platform == "Application":
-        print('Get request from Application')
-        appCtrl = AppCtrl("VideoApp")
-        data2send = appCtrl.SplitVideo(request, False)
-    else: 
-        data2send = {
-            'status': 400,
-            'message': "Неверные параметры запроса",
-        }
-    print(data2send)
+    print('Get request from Application')
+    appCtrl = AppCtrl("VideoApp")
+    data2send = appCtrl.SplitVideo(request, False)
     return jsonify(data2send)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=port)
+    # thread1 = threading.Thread(target=lambda: geosystemMobileApp.run(debug=True, port=portMobile))
+    # thread2 = threading.Thread(target=lambda: unityApp.run(debug=True, port=portUnity))
+    # thread3 = threading.Thread(target=lambda: tkinterApp.run(debug=True, port=portTkinter))
+    # thread1.start()
+    # thread2.start()
+    # thread3.start()
+    geosystemMobileApp.run(debug=True, port=portMobile)
