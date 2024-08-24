150205a83d07defa1a53607236ad15d6a17a413c
AlexGat27 <aleksandrgataulin745@gmail.com>
2024-08-24 13:55:51 +0300

    Создание главного файла

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from applications.factories import FlaskAppFactory
+from applications.factories import FlaskAppFactory, KafkaAppFactory
 from applications.ApplicationEnums import AppNames
 
 def main():
@@ -9,9 +9,10 @@ def main():
 
     # Получение значения переменной окружения для типа приложения
     app_name = os.getenv('APP_TYPE')
+    microservice_name = os.getenv('MICROSERVICE_TYPE')
 
-    if not app_name:
-        print("Ошибка: Переменная окружения 'APP_TYPE' не установлена.")
+    if not app_name or not microservice_name:
+        print("Ошибка: Необходимые переменные окружения не установлены.")
         return
 
     app_name = app_name.upper()
@@ -20,7 +21,14 @@ def main():
         print(f"Ошибка: Неизвестное приложение '{app_name}'.")
         return
 
-    app_factory = FlaskAppFactory()
+    if (microservice_name == "flask"):
+        app_factory = FlaskAppFactory()
+    elif (microservice_name == "kafka"):
+        app_factory = KafkaAppFactory()
+    else:
+        print(f"Ошибка: Такого типа микросервисв не существует '{microservice_name}'.")
+        return
+    
     app_instance = app_factory.create_app(AppNames[app_name])
     app_instance.run()
 
